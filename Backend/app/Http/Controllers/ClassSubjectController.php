2dd4961f0a36f8d1474eeb6f3f7859a1d22195ae
haju35 <hajirasiraj217@gmail.com>
2026-03-10 05:49:04 -0700

    folder structure

---
+++ Backend/app/Http/Controllers/ClassSubjectController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassSubject;
+use Illuminate\Http\Request;
+
+class ClassSubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClassSubject $classSubject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClassSubject $classSubject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ClassSubject $classSubject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClassSubject $classSubject)
+    {
+        //
+    }
+}
